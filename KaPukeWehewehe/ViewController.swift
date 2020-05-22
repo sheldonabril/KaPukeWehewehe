f136062433861e5ab9a995a2bdeded75f046ac3f
Sheldon Abril <sheldon.abril@gmail.com>
2020-05-21 19:14:10 -1000

    hoʻomaka i ka puke wehewehe

--- KaPukeWehewehe/ViewController.swift
+++ KaPukeWehewehe/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+{
+    var arrNaHuaOlelo = ["EA", "KUOKOA"]
+    var arrWeheweheAna = ["sovereignty", "independence"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return arrNaHuaOlelo.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")
+        let text = arrNaHuaOlelo[indexPath.row]
+        cell?.textLabel?.text = text
+        cell?.detailTextLabel?.text = arrWeheweheAna[indexPath.row]
+        
+        return cell!
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
